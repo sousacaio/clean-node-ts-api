c7bc9c24e6ccb9f75d822aff46e6743c3b75b906
caio <frias.caio@yahoo.com.br>
2021-11-07 17:36:08 -0300

    test: ensure SignUpController return an error no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -15,4 +15,18 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('missigin param: name'))
   })
+  test('Should return 400 if no email is provided', () => {
+    // sut significa system under test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missigin param: email'))
+  })
 })
